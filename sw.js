0104bb30118c0e1f913d9ed7ebd946f1f5dd221b
BentaTG <tralmagutierrez@gmail.com>
2026-08-04 04:49:29 -0400

    Update SW to clear old caches and use network-first strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tarjeta-clara-v1';
+const CACHE_NAME = 'gastos-santander-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -13,14 +13,26 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cache or fetch network
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
